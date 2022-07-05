467294f6b0a9c11de1cacf1e9052ae42f0513ccb
Gonzo <gonzalo.araosolguin@gmail.com>
2022-07-05 11:11:58 -0400

    Tienda de Juegos 32 bits P2

--- src/components/DatosJuegos.vue
+++ src/components/DatosJuegos.vue
@@ -1,38 +1,68 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <ul>
-      <li v-for="dato in juegos" :key="dato">{{dato.nombre}}</li>
-    </ul>
+    <br>
     <div id="tabla">
       <table class="table table-bordered border-primary">
         <tr>
           <th scope="col">Código</th>
+          <th scope="col">Color</th>
           <th scope="col">Nombre</th>
           <th scope="col">Stock</th>
           <th scope="col">Precio</th>
+          <th scope="col">Acciones</th>
         </tr>
-        <tr v-for="registro in juegos" :key="registro" :style="`color: ${registro.color}`">
-          <td>{{registro.codigo}}</td>
-          <td>{{registro.nombre}}</td>
-          <td>{{registro.stock}}</td>
-          <td>{{registro.precio}}</td>
+        <tr v-for="juego in juegos" :key="juego" :style="`color: ${juego.color}`">
+          <td>{{juego.codigo}}</td>
+          <td>
+            <select v-on:change="elegirColor($event, juego.codigo); actionColor(color)">
+              <option value=""></option>
+              <option value="yellow">Amarillo</option>
+              <option value="aqua">Celeste</option>
+              <option value="lightgreen">Verde</option>
+              <option value="white">Blanco</option>
+            </select>
+          </td>
+          <td>{{juego.nombre}}</td>
+          <td>{{juego.stock}}</td>
+          <td>{{juego.precio}}</td>
+          <td>
+            <button @click="masJuegos(juego.codigo)"><i class="fa-solid fa-angle-up"></i></button>
+            <button @click="menosJuegos(juego.codigo)"><i class="fa-solid fa-angle-down"></i></button>
+          </td>
         </tr>
       </table>
     </div>
+    <br><hr>
+    <h2>Total: {{totalDeJuegos}}</h2>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState, mapGetters, mapMutations, mapActions } from 'vuex'
 export default {
   name: 'DatosJuegos',
   props: {
     msg: String
   },
+  data() {
+    return {
+      color: {}
+    }
+  },
   computed:{
-    ...mapState(["juegos"]),
-  }
+    ...mapState(["juegos", "cantidadDeJuegos"]),
+    ...mapGetters(["totalDeJuegos"])
+  },
+  methods: {
+    ...mapMutations([]),
+    ...mapActions(["masJuegos", "menosJuegos", "actionColor"]),
+    elegirColor(event, codigo) {
+      let colorEvento = event.target.value;
+      this.color = {"color" : colorEvento, "codigo" : codigo}
+      return this.color
+    }
+  },
 }
 </script>
 
@@ -55,11 +85,20 @@ a {
 
 #tabla {
   text-align: center;
-  width: 50%;
+  width: 70%;
   display: inline-flex;
 }
 
 #tabla th {
   color: #FFF;
+}
+
+select, button {
+  background-color: #FFF;
+}
+
+hr {
+  width: 70%;
+  display: inline-flex;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -6,13 +6,57 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    juegos
+    juegos,
+    cantidadDeJuegos: 0,
   },
   getters: {
+    totalDeJuegos(state) {
+      let cantidadDeJuegos = 0;
+      for (let juego of state.juegos) {
+        cantidadDeJuegos += parseInt(juego.stock);
+      }
+      return cantidadDeJuegos;
+    }
   },
   mutations: {
+    masJuegos(state, codigo) {
+      for(let i=0; i < state.juegos.length; i++){
+        if(codigo === state.juegos[i].codigo){
+          let cantidad = parseInt(state.juegos[i].stock);
+          cantidad += 1
+          state.juegos[i].stock = cantidad.toString();
+        }
+      }
+    },
+    menosJuegos(state, codigo) {
+      for(let i=0; i < state.juegos.length; i++){
+        if(codigo === state.juegos[i].codigo) {
+          let cantidad = parseInt(state.juegos[i].stock);
+          cantidad += -1
+          state.juegos[i].stock = cantidad.toString();
+        }
+      }
+    },
+    cambioColor(state, colorCodigo) {
+      let cambioColor = colorCodigo.color;
+      let codigoColor = colorCodigo.codigo;
+      for(let i=0; i < state.juegos.length; i++) {
+        if(codigoColor === state.juegos[i].codigo) {
+          state.juegos[i].color = cambioColor;
+        }
+      }
+    }
   },
   actions: {
+    masJuegos({commit}, codigo) {
+      commit("masJuegos", codigo)
+    },
+    menosJuegos({commit}, codigo) {
+      commit("menosJuegos", codigo)
+    },
+    actionColor({commit}, colorCodigo) {
+      commit("cambioColor", colorCodigo)
+    }
   },
   modules: {
   }
